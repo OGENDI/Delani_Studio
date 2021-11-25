8464f12a20c0fe1d37fc53be173f76183378cb53
OGENDI <demokonge17@gmail.com>
2021-11-26 00:15:51 +0300

    It will style the development text

---
+++ delani.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    $('#desigining').click(function(){
+        $('#design').show();
+    });
+    $('#design').click(function(){
+        $('#design').slideUp();
+    });
+});
+
+$(document).ready(function(){
+    $('#develop').click(function(){
+        $('#development').show();
+    });
+    $('#development').click(function(){
+        $('#development').slideUp();
+    });
+});
+
+
+
+
+userForm.addEventListener('submit',function(event) {
+   event.preventDefault();
+   var inputName = $('#userName').val();
+   var userEmail = $('email').val();
+   var userMessage =$('message').val();
+
+   if(inputName !== '' && userEmail !== '' && userMessage !== ''){
+       alert(`Hello ${inputName}. We have recieved your feedback and we will get back to you.`);
+       
+   } else {
+       alert('Please fill all fields');
+   }
+ 
+})
